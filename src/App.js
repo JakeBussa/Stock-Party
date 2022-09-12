0841c5ce1f831d4551a4e118987fe84e1a73859b
DeliciousWaffle <jakebussa96@gmail.com>
2022-09-11 21:21:45 -0600

    Added the Stock Details section and spiced things up with some css.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Heading from "./components/Heading.js";
 import StockDetails from "./components/StockDetails.js";
 import StockGraph from "./components/StockGraph";
 import PerformanceDetails from "./components/PerformanceDetails";
+import "./App.css"
 
 function App() {
   return (
