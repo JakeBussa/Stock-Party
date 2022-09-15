617f2c67d80c8ab5e3c6a98332b748f416e0a17a
DeliciousWaffle <jakebussa96@gmail.com>
2022-09-14 18:53:53 -0600

    Random updates and figuring out best way to handle input section

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -12,28 +12,29 @@ class Heading extends React.Component {
           with other investments too! 😎 With this little app, you will be able to select an investment and see how
           well it performed with a given date range. Here's some info to get you started:
         </p>
-        <ul>
+        <ul className="button-info-list">
           <li>
-            Clicking the <span className="app-info">Stocks</span> button will list the available stocks to choose from.
+            Clicking the <span className="button-info">Stocks</span> button will list the available stocks to choose
+            from.
           </li>
           <li>
-            The <span className="app-info">Amount</span> field is the number of stocks that you wish to purchase.
+            The <span className="button-info">Amount</span> field is the number of stocks that you wish to purchase.
           </li>
           <li>
-            Clicking the <span className="app-info">Begin Year</span> button will drop down a list of years that the
+            Clicking the <span className="button-info">Begin Year</span> button will drop down a list of years that the
             stock has existed for. The year entered represents the date in which you purchased a particular investment.
           </li>
           <li>
-            Similarly, the <span className="app-info">End Year</span> button will list the years after the&nbsp;
-            <span className="app-info">Begin Year</span> that the stock has existed for. This represents the day in
+            Similarly, the <span className="button-info">End Year</span> button will list the years after the&nbsp;
+            <span className="button-info">Begin Year</span> that the stock has existed for. This represents the day in
             which you sold an investment.
           </li>
         </ul>
         <p>
           The line graph below shows how the stock performed over time. When the&nbsp;
-          <span className="app-info">Begin Year</span> and the <span className="app-info">End Year</span> are selected,
-          vertical lines are drawn to highlight these years. Below that, you can see how well that investment performed
-          and what it's worth now.
+          <span className="button-info">Begin Year</span> and the <span className="button-info">End Year</span> are
+          selected, vertical lines are drawn to highlight these years. Below that, you can see how well that investment
+          performed and what it's worth now.
         </p>
       </>
     );
--- src/components/StockDetails.js
+++ src/components/StockDetails.js
@@ -1,11 +1,66 @@
 import React from "react";
+import "./StockDetails.css"
 
-class StockDetails extends React.Component {
-  render() {
-    return <p>
-      Stock Details!
-    </p>
+export default class StockDetails extends React.Component {
+  render () {
+    return (
+      <ul className="">
+        <li>
+          <StockPicker stockSymbols={["Stock 1", "Stock 2", "Stock 3"]} />
+        </li>
+        <li>
+          <StockAmount />
+        </li>
+        <li>
+          <BeginYear />
+        </li>
+        <li>
+          <EndYear />
+        </li>
+      </ul>
+    );
   }
 }
 
-export default StockDetails;
+const StockPicker = (props) => {
+  return (
+    <select>
+      {
+        props.stockSymbols.map(stockSymbol =>
+          <option>
+            {stockSymbol}
+          </option>
+        )
+      }
+    </select>
+  );
+}
+
+function StockAmount (props) {
+  return (
+    <div className="stockAmount">
+      <label>Stock Amount</label>
+      <input type="text" />
+    </div>
+  );
+}
+
+function BeginYear (props) {
+  return (
+    <>
+      <label>
+        Begin Year
+      </label>
+    </>
+  );
+}
+
+function EndYear (props) {
+  return (
+    <>
+      <label>
+        End Year
+      </label>
+    </>
+  );
+}
