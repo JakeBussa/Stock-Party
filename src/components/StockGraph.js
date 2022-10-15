4b39df549d5fa553779cb183478e8901d2303c19
DeliciousWaffle <jakebussa96@gmail.com>
2022-10-15 16:08:04 -0600

    Get header set up

--- src/components/StockGraph.js
+++ src/components/StockGraph.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-class StockGraph extends React.Component {
+export default class StockGraph extends React.Component {
+  getStockNameFromSymbol = targetStockSymbol => {
+    const stockData = this.props.data.stockData;
+    return stockData
+      .find(datum => datum.stockSymbol === targetStockSymbol)
+      .stockName;
+  }
+
   render() {
+    const { selectedStockSymbol } = this.props.data;
+    const stockName = this.getStockNameFromSymbol(selectedStockSymbol);
+
     return (
-      <div style={{ backgroundColor: "rgb(0,255,0)" }}>
-        <p>
-          Stock Graph!
-        </p>
+      <div style={{ backgroundColor: "rgb(0,255,0)", textAlign: "center"}}>
+        <h2 style={{ fontSize: "50px" }}>
+          {stockName} Performance
+        </h2>
       </div>
     );
   }
 }
-
-export default StockGraph;
